5de867637cc64e69fc25189c3f7ac18594f2e56a
Simon Yoo <yoosghn0@gmail.com>
2023-11-23 12:23:41 +1300

    css

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,36 +8,57 @@ import {
 function Footer() {
   return (
     <>
-      <div className='flex flex-col items-center'>
+      <div className='w-full'>
+        <hr className='m-12' />
+      </div>
+      <div className='flex justify-center items-center'>
         <hr />
-        <div className='icon-links'>
-          <a className='icon' href='https://www.instagram.com/simonshyoo/'>
-            {' '}
-            <span>
-              <AiOutlineInstagram size={30} color={'#424445'} />
-            </span>{' '}
-            <span>Instagram</span>
-          </a>
+        <div className='flex justify-center w-full'>
           <a
-            className='icon'
+            className='relative p-12 w-[100px] h-[100px] rounded-2xl overflow-hidden cursor-pointer transition-all duration-700 card group'
+            href='https://www.instagram.com/simonshyoo/'
+          >
+            <div className='opacity-100 absolute inset-0 w-full h-full flex justify-center items-center transition-all duration-100 delay-200 z-20 group-hover:opacity-0'>
+              <AiOutlineInstagram size={30} color={'#424445'} />
+            </div>
+            <div className='opacity-0 absolute inset-0 w-full h-full flex justify-center items-center transition-all duration-300 delay-200 z-10 group-hover:opacity-100 card-back uppercase'>
+              Instagram
+            </div>
+          </a>
+
+          <a
+            className='relative p-12 w-[100px] h-[100px] rounded-2xl overflow-hidden cursor-pointer transition-all duration-700 card group'
             href='https://www.linkedin.com/in/simon-yoo-12b078183/'
           >
-            <span>
+            <div className='opacity-100 absolute inset-0 w-full h-full flex justify-center items-center transition-all duration-100 delay-200 z-20 group-hover:opacity-0'>
               <AiOutlineLinkedin size={30} color={'#424445'} />
-            </span>{' '}
-            <span>LinkedIn</span>
+            </div>
+            <div className='opacity-0 absolute inset-0 w-full h-full flex justify-center items-center transition-all duration-300 delay-200 z-10 group-hover:opacity-100 card-back uppercase'>
+              LinkedIn
+            </div>
           </a>
-          <a className='icon' href='https://github.com/simon-yoo'>
-            <span>
+
+          <a
+            className='relative p-12 w-[100px] h-[100px] rounded-2xl overflow-hidden cursor-pointer transition-all duration-700 card group'
+            href='https://github.com/simon-yoo'
+          >
+            <div className='opacity-100 absolute inset-0 w-full h-full flex justify-center items-center transition-all duration-100 delay-200 z-20 group-hover:opacity-0'>
               <AiOutlineGithub size={30} color={'#424445'} />
-            </span>
-            <span>Github</span>
+            </div>
+            <div className='opacity-0 absolute inset-0 w-full h-full flex justify-center items-center transition-all duration-300 delay-200 z-10 group-hover:opacity-100 card-back uppercase'>
+              Github
+            </div>
           </a>
-          <a className='icon' href='https://www.facebook.com/yoosghn0'>
-            <span>
+          <a
+            className='relative p-12 w-[100px] h-[100px] rounded-2xl overflow-hidden cursor-pointer transition-all duration-700 card group'
+            href='https://www.facebook.com/yoosghn0'
+          >
+            <div className='opacity-100 absolute inset-0 w-full h-full flex justify-center items-center transition-all duration-100 delay-200 z-20 group-hover:opacity-0'>
               <AiOutlineFacebook size={30} color={'#424445'} />
-            </span>
-            <span>Facebook</span>
+            </div>
+            <div className='opacity-0 absolute inset-0 w-full h-full flex justify-center items-center transition-all duration-300 delay-200 z-10 group-hover:opacity-100 card-back uppercase'>
+              Facebook
+            </div>
           </a>
         </div>
       </div>
