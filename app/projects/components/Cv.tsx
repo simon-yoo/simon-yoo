4f02be03c334f84957219c16dd4e1802d85b2f7e
Simon Yoo <yoosghn0@gmail.com>
2023-11-26 11:56:30 +1300

    fixing error

---
+++ app/projects/components/Cv.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Link } from 'react-router-dom'
+
+const Cv = () => {
+  return (
+    <a download='/CVsimon.pdf' target='_blank' rel='noopener noreferrer'>
+      Download CV
+    </a>
+  )
+}
+
+export default Cv
